d728ba46885365a2d659ca42b6919cb4fe1583d1
daniel rodriguez <daniel.rodriguez@booking.com>
2020-01-24 12:53:27 +0100

    made all form fields except name not required

--- hoy_comemos_django/app/models.py
+++ hoy_comemos_django/app/models.py
@@ -26,14 +26,14 @@ duration_choices = [
 
 class Meal(models.Model):
     name = models.CharField(default="",max_length=264, unique=True)
-    category = models.CharField(default="",max_length=264, choices=category_choices)
-    ingredients = models.TextField(default="")
-    complexity = models.CharField(default="",max_length=264, choices=complexity_choices)
-    duration = models.CharField(default="",max_length=264, choices=duration_choices)
-    link = models.URLField(default="")
-    is_new = models.BooleanField(default=True)
-    image_name = models.CharField(default="",max_length=264)
-    image = models.ImageField(upload_to='images')
+    category = models.CharField(default="",max_length=264, choices=category_choices, blank = True)
+    ingredients = models.TextField(blank = True)
+    complexity = models.CharField(default="",max_length=264, choices=complexity_choices, blank = True)
+    duration = models.CharField(default="",max_length=264, choices=duration_choices, blank = True)
+    link = models.URLField(default="", blank = True)
+    is_new = models.BooleanField(default=True, blank = True)
+    image_name = models.CharField(default="",max_length=264, blank = True)
+    image = models.ImageField(upload_to='images', blank = True)
 
     def __str__(self):
         return self.name
