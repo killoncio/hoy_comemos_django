2680f8eab7785e8394519d556b1716500ea05489
Daniel Rodriguez <daniel.rodriguez@booking.com>
2020-12-27 23:13:02 +0100

    added back delimitators

--- hoy_comemos_django/static/app/js/app_vue.js
+++ hoy_comemos_django/static/app/js/app_vue.js
@@ -1,26 +1,27 @@
 Vue.component('receipt-item', {
-  props: ['receipt'],
-  template:`
-  	<li
-  		:class="{receipt: true, receipt__new: receipt.is_new}"
-  		:data-category="receipt.category"
-  		draggable="true"
-  	>
+	delimiters: ['[[', ']]'],
+	props: ['receipt'],
+	template:`
+		<li
+			:class="{receipt: true, receipt__new: receipt.is_new}"
+			:data-category="receipt.category"
+			draggable="true"
+		>
 		<div class="receipt_name">
-			<a class="text-primary" href="{% url 'app:meal' meal.id %}" target='_blank'><strong>{{receipt.name}}</strong></a>
+			<a class="text-primary" href="{% url 'app:meal' meal.id %}" target='_blank'><strong>[[receipt.name]]</strong></a>
 			<span v-if="receipt.ingredients" class="glyphicon glyphicon glyphicon-menu-right ingredients_trigger" aria-hidden="true"></span>
 		</div>
 		<div class='receipt_image img-wrapper' style="position: relative">
-    		<img class='img-responsive'
-    			:src="receipt.image.url"
-    			alt="Uh Oh, didn't show!"
-    		>
+			<img class='img-responsive'
+				:src="[[receipt.image]]"
+				alt="Uh Oh, didn't show!"
+			>
 		</div>
 		<div class="receipt_ingredients hide">
-			{{receipt.ingredients}}
+			[[receipt.ingredients]]
 		</div>
 	</li>
-  	`
+		`
 });
 
 var vm = new Vue({
